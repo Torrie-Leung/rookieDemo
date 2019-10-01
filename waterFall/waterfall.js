14866466fcd0fb65dd607e4a6843eb30e0aacd23
TorrieL <hmminicity2@163.com>
2019-10-01 18:58:55 +0800

    scroll

--- waterFall/waterfall.js
+++ waterFall/waterfall.js
@@ -1,31 +1,53 @@
 window.onload = function () {
   waterfall()
+  const dataImg ={
+    "data" :[
+      {src:"01.jpeg"},
+      {src:"02.jpeg"},
+      {src:"03.jpeg"},
+      {src:"04.jpeg"},
+      {src:"05.jpeg"},
+      {src:"06.jpeg"},
+      {src:"07.jpeg"},
+      {src:"08.jpeg"},
+      ]
+  }
 }
 
 function waterfall(){
-  let onePin = get();
-  let iPinW = onePin[0].offsetWidth;
+  let pinArr = get();
+  //get item's width
+  let iPinW = pinArr[0].offsetWidth;
+  //the number of displaying column = pageWidth/itemWidth
   let num =Math.floor(document.documentElement.clientWidth/iPinW) 
-  console.log(num)
   const parent = document.getElementById('main')
-  console.log(parent.style)
   parent.style.cssText = 'width:'+iPinW*num+'px;margin:0 auto;';
+
   let pinHArr= []
-  for(let i = 0; i<onePin.length; i++){
-    let pinH = onePin[i].offsetHeight
+  
+  for(let i = 0; i<pinArr.length; i++){
+    let pinH = pinArr[i].offsetHeight
+    //console.log(pinH)
     if(i<num){
       pinHArr[i] = pinH
+      //console.log(pinHArr)
     }else{
-      
+      //第一行的最小高度
       let minH = Math.min.apply(null,pinHArr)
+      //第一行的最小高度index
       let minHIndex = getMinHIndex(pinHArr,minH)
-      onePin[i].style.position = 'absolute'
-      onePin[i].style.top = minH + 'px'
-      onePin[i].style.left = onePin[minHIndex].offsetLeft+'px'
-      pinHArr[minHIndex]+=onePin[i].offsetHeight
-      console.log(pinHArr[minHIndex])
+      //console.log(i+"-"+minHIndex)
+      pinArr[i].style.position = 'absolute'
+      pinArr[i].style.top = minH + 'px'
+      pinArr[i].style.left = pinArr[minHIndex].offsetLeft+'px'
+      //console.log(pinHArr[minHIndex])
+      pinHArr[minHIndex]+=pinArr[i].offsetHeight
+      
+      
     }
   }
+  
+  
 }
 
 function get(){
@@ -34,7 +56,6 @@ function get(){
   for(let i = 0; i< obj.length; i++){
     pinArr.push(obj[i]);
   }
-  console.log(pinArr[0])
   return pinArr
 }
 
@@ -46,3 +67,10 @@ function getMinHIndex(arr,minH){
   }
 }
 
+function checkScroll(){
+  const aPin = document.getElementsByTagName('pin')
+  const lastPinH = aPin[aPin.length - 1].offsetTop - (Math.floor(aPin[aPin.length - 1].offsetHeight/2))
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+  const documentH = document.documentElement.clientHeight
+  return(lastPinH<scrollTop+documentH)?true:false
+}
