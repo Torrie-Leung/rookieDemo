1a744bb3ef05b6543ea274f5b9479c5f1c4d23a2
TorrieL <hmminicity2@163.com>
2019-10-04 12:21:30 +0800

    ake fun of Dr.Z

--- picSwap/swap.js
+++ picSwap/swap.js
@@ -1,3 +1,23 @@
+const DrZsHoliday = ['1stOct','2ndOct','3rdOct','4thOct','5thOct','6thOct','7thOct']
+for(i = 0; i<DrZsHoliday.length; i++){
+  eatFishJing(i)
+}
+function eatFishJing(i){
+  if(i<3){
+    console.log('国庆节第'+(i+1)+'天，'+'老赵在吃劲仔小鱼')
+  }
+  if(i>=3){
+    console.log('国庆节第'+(i+1)+'天，'+'老赵还在吃劲仔小鱼')
+  }
+  
+}
+
+
+
+
+
+
+
 window.onload = function () {
   const imgSrc = {
     "data" :[
@@ -17,14 +37,14 @@ window.onload = function () {
   const img = document.getElementsByTagName('img')[0]
   let idx = 0
   const info = document.getElementById('info')
-  info.innerHTML = "Total "+ imgSrc.data.length + ' pics'+'-'+[idx+1]
+  info.innerHTML = "Total "+ imgSrc.data.length + ' pics'+'-'+[idx+1]+'.jpeg'
   prev.onclick = function () {
     idx--;
     if(idx<0){
       idx = imgSrc.data.length-1
     }
     img.src = "./images/" + imgSrc.data[idx]
-    info.innerHTML = "Total "+ imgSrc.data.length + ' pics'+'-'+[idx+1]
+    info.innerHTML = "Total "+ imgSrc.data.length + ' pics'+'-'+[idx+1]+'.jpeg'
   }
 
   next.onclick = function () {
@@ -33,6 +53,6 @@ window.onload = function () {
       idx = 0
     }
     img.src = "./images/" + imgSrc.data[idx]
-    info.innerHTML = "Total "+ imgSrc.data.length + ' pics'+'-'+[idx+1]
+    info.innerHTML = "Total "+ imgSrc.data.length + ' pics'+'-'+[idx+1]+'.jpeg'
   }
 }
