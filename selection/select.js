4f18de4ed7cd68761869be652f4399a80e6337d1
TorrieL <hmminicity2@163.com>
2019-10-05 14:36:43 +0800

    cancel all btn

--- selection/select.js
+++ selection/select.js
@@ -12,4 +12,14 @@ window.onload = function(){
       items[i].checked = !items[i].checked
     }
   })
+
+  //cancel all btn
+  btnClick('cancleAllBtn',function(){
+    const items = document.getElementsByName('items')
+    for(i = 0; i<items.length; i++){
+      if(items[i].checked){
+        items[i].checked = false
+      }
+    }
+  })
 }
