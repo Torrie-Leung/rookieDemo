7e7aa2301d482076cff63789df3c6e6d8a9a9b47
TorrieL <hmminicity2@163.com>
2019-10-07 22:35:32 +0800

    to left

--- movingBox/movingBox.js
+++ movingBox/movingBox.js
@@ -1,16 +1,42 @@
 window.onload = function () {
   const box1 = document.querySelector('#box1')
   const btn2R = document.querySelector('#btn2R')
+  const btn2L = document.querySelector('#btn2L')
+  let timer
 
   btn2R.onclick = function() {
+    clearInterval(timer)
     //create a timer
-    let timer =setInterval(function(){
+    timer =setInterval(function(){
       const oldV = parseInt(getStyle(box1,"left"))
       
-      let newV = oldV + 10
+      let newV = oldV + 13
       box1.style.left = newV + 'px'
 
+      if (newV >400){
+        newV = 400
+      }
+
       if(newV == 400){
+        clearInterval(timer)
+      }
+    },30)
+  }
+
+  btn2L.onclick = function() {
+    clearInterval(timer)
+    //create a timer
+    timer =setInterval(function(){
+      const oldV = parseInt(getStyle(box1,"left"))
+      
+      let newV = oldV - 13
+      box1.style.left = newV + 'px'
+
+      if (newV <0){
+        newV = 0
+      }
+
+      if(newV == 0){
         clearInterval(timer)
       }
     },30)
