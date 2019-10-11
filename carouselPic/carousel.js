99c42e9f3072f795b4401a04c012ae46ff99ecc9
TorrieL <hmminicity2@163.com>
2019-10-11 23:13:11 +0800

    css handled by js

--- carouselPic/carousel.js
+++ carouselPic/carousel.js
@@ -3,4 +3,8 @@ window.onload = function () {
   const imgList = document.getElementById('imgList')
   const imgArr = document.getElementsByTagName('img')
   imgList.style.width = imgArr.length * 245 + 'px'
+  //set nav to the middle
+  const navRect = document.getElementById('navRect')
+  const outer = document.getElementById('outer')
+  navRect.style.left = (outer.offsetWidth-navRect.offsetWidth)/2 + 'px'
 }
