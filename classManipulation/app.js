3b2311454bad129c0677de44190807fdb5d0c19e
TorrieL <hmminicity2@163.com>
2019-10-19 23:38:15 +0800

    use reg as a filter

--- classManipulation/app.js
+++ classManipulation/app.js
@@ -2,6 +2,19 @@ window.onload = function () {
   const btn = document.getElementById('btn')
   const box = document.getElementById('box')
   btn.onclick = function () {
-    box.className = "b2"
+    //box.className = "b2"
+    if(!hasClass(box,"b2")){
+      addClass(box,"b2")
+    }
+    
+    
+  }
+  function addClass(obj,cn){
+    obj.className +=' '+cn
+  }
+  function hasClass(obj,cn){
+    const reg = new RegExp("\\b"+cn+"\\b")
+
+    return reg.test(obj.className)
   }
 }
