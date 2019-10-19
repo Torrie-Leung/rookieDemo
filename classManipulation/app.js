03669a95f68d0d14308f71fb0fb85b880ae1341c
TorrieL <hmminicity2@163.com>
2019-10-19 22:52:46 +0800

    manipulate css class by js

---
+++ classManipulation/app.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  const btn = document.getElementById('btn')
+  const box = document.getElementById('box')
+  btn.onclick = function () {
+    box.className = "b2"
+  }
+}
