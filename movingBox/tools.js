341142c110f6068548231ccf249ec09ab7369852
TorrieL <hmminicity2@163.com>
2019-10-19 23:53:33 +0800

    more tools

--- movingBox/tools.js
+++ movingBox/tools.js
@@ -23,15 +23,37 @@ function moveBox(obj,attr,speed,target,callback){
     },30)
   }
 
-  function getStyle(obj,name){
-    if(window.getComputedStyle){
-      //getComputedStyle() gives the final used values of all the CSS properties of an element.
-      //read only, cannot be revised
-      //getComputedStyle(element,psuedo)
-      //the other way--getComputedStyle(element,psuedo).getPropertyValue('name') *kebab-case
-      return getComputedStyle(obj,null)[name]
-    }else{
-      //cater for IE 8
-      return obj.currentStyle[name]
-    }
+function getStyle(obj,name){
+  if(window.getComputedStyle){
+    //getComputedStyle() gives the final used values ofall the CSS properties of an element.
+    //read only, cannot be revised
+    //getComputedStyle(element,psuedo)
+    //the other way--getComputedStyle(element,psuedo.getPropertyValue('name') *kebab-case
+    return getComputedStyle(obj,null)[name]
+  }else{
+    //cater for IE 8
+    return obj.currentStyle[name]
   }
+}
+
+function addClass(obj,cn){
+  obj.className +=' '+cn
+}
+
+function hasClass(obj,cn){
+  const reg = new RegExp("\\b"+cn+"\\b")
+  return reg.test(obj.className)
+}
+
+function rmvClass(obj,cn){
+  const reg = new RegExp("\\b"+cn+"\\b")
+  obj.className = obj.className.replace(reg,'')
+}
+
+function tglClass(obj,cn){
+  if(hasClass(obj,cn)){
+    rmvClass(obj,cn)
+  }else{
+    addClass(obj,cn)
+  }
+}
