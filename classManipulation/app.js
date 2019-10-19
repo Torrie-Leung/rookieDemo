d6ccf19342057e0118b672b82a9d9ca75f10810f
TorrieL <hmminicity2@163.com>
2019-10-19 23:48:35 +0800

    remove & toggle class

--- classManipulation/app.js
+++ classManipulation/app.js
@@ -1,5 +1,7 @@
 window.onload = function () {
   const btn = document.getElementById('btn')
+  const rbtn = document.getElementById('rbtn')
+  const tglbtn = document.getElementById('tglbtn')
   const box = document.getElementById('box')
   btn.onclick = function () {
     //box.className = "b2"
@@ -9,6 +11,13 @@ window.onload = function () {
     
     
   }
+  rbtn.onclick = function () {
+    rmvClass(box,"b2")
+    
+  }
+  tglbtn.onclick= function () {
+    tglClass(box,"b2")
+  }
   function addClass(obj,cn){
     obj.className +=' '+cn
   }
@@ -16,5 +25,16 @@ window.onload = function () {
     const reg = new RegExp("\\b"+cn+"\\b")
 
     return reg.test(obj.className)
+  }
+  function rmvClass(obj,cn){
+    const reg = new RegExp("\\b"+cn+"\\b")
+    obj.className = obj.className.replace(reg,'')
+  }
+  function tglClass(obj,cn){
+    if(hasClass(obj,cn)){
+      rmvClass(obj,cn)
+    }else{
+      addClass(obj,cn)
+    }
   }
 }
