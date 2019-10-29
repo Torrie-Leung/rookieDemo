4438c7103ae46fbbf984cb092bf29bd5d7976865
TorrieL <hmminicity2@163.com>
2019-10-29 23:00:24 +0800

    sorting

--- dynamicTable/table.js
+++ dynamicTable/table.js
@@ -16,6 +16,27 @@ function loadTable(aceData) {
   for(let ace of aceData){
     dataHtml += `<tr><td>${ace.name}</td><td>${ace.level}</td></tr>`;
   }
-  console.log(dataHtml)
+  //console.log(dataHtml)
   tableBody.innerHTML = dataHtml
+}
+
+function sortColumn(columnName) {
+  const dataType = typeof aceData[0][columnName];
+  
+  sortDirection = !sortDirection;
+
+  switch(dataType){
+    
+    case "number":
+      sortNumberColumn(sortDirection,columnName);
+      break
+  }
+  //console.log(aceData)
+  loadTable(aceData)
+}
+
+function sortNumberColumn(sort,columnName) {
+  aceData = aceData.sort((a1,a2) => {
+    return sort ? a1[columnName] - a2[columnName]:a2[columnName] - a1[columnName]
+  })
 }
