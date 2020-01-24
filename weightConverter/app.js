39eaee544eebdd85c06d17e50f64341dd1f3e587
TorrieL <hmminicity2@163.com>
2020-01-24 11:34:34 +0800

    transfer lbs to g

--- weightConverter/app.js
+++ weightConverter/app.js
@@ -1,4 +1,8 @@
 const lbsInput = document.getElementById('lbsInput')
-lbsInput.addEventListener('input',function(){
-  console.log('kkk')
+const gramsOutput = document.getElementById('gramsOutput')
+const kgramsOutput = document.getElementById('kgramsOutput')
+const ozOutput = document.getElementById('ozOutput')
+lbsInput.addEventListener('input',function(e){
+  let lbs = e.target.value
+  gramsOutput.innerHTML = lbs/0.0022046
 })
