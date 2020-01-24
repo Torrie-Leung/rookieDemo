3d3f4c3c8f272bce77897ab3c3399358d7e85c00
TorrieL <hmminicity2@163.com>
2020-01-24 11:39:51 +0800

    visibility hidden/visible

--- weightConverter/app.js
+++ weightConverter/app.js
@@ -2,7 +2,10 @@ const lbsInput = document.getElementById('lbsInput')
 const gramsOutput = document.getElementById('gramsOutput')
 const kgramsOutput = document.getElementById('kgramsOutput')
 const ozOutput = document.getElementById('ozOutput')
+const output = document.getElementById('output')
+output.style.visibility = 'hidden'
 lbsInput.addEventListener('input',function(e){
+  output.style.visibility = 'visible'
   let lbs = e.target.value
   gramsOutput.innerHTML = lbs/0.0022046
   kgramsOutput.innerHTML = lbs/2.2046
