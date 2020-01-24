9a71106d3290f34f805eab979617a360cb8b05b5
TorrieL <hmminicity2@163.com>
2020-01-24 11:36:42 +0800

    transfer libs to kg and onces

--- weightConverter/app.js
+++ weightConverter/app.js
@@ -5,4 +5,6 @@ const ozOutput = document.getElementById('ozOutput')
 lbsInput.addEventListener('input',function(e){
   let lbs = e.target.value
   gramsOutput.innerHTML = lbs/0.0022046
+  kgramsOutput.innerHTML = lbs/2.2046
+  ozOutput.innerHTML = lbs*16
 })
