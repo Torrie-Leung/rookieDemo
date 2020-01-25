03ab7337605e62720c36d19b4f3cba2a99b8948a
TorrieL <hmminicity2@163.com>
2020-01-25 18:11:50 +0800

    Init TypeWriter

--- TypeWriterEffect/main.js
+++ TypeWriterEffect/main.js
@@ -7,3 +7,17 @@ const TypeWriter = function (txtEle,words,waitTime = 3000) {
   this.type();
   this.isDeleting = false
 }
+
+//Type Method
+
+//Init on DOM Load
+document.addEventListener('DOMContentLoaded',init)
+
+//Init App
+function init() {
+  const txtEle = document.querySelector('.txt-type')
+  const words = JSON.parse(txtEle.getAttribute('data-words')) 
+  const waitTime = txtEle.getAttribute('data-wait')
+  //Init TypeWriter
+  new TypeWriter(txtEle,words,waitTime)
+}
