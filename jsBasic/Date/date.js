032e08613df7dfe4c17616a81ba2991ddf3c9393
TorrieL <hmminicity2@163.com>
2020-03-13 01:12:45 +0800

    repair 0

--- jsBasic/Date/date.js
+++ jsBasic/Date/date.js
@@ -1,7 +1,7 @@
 function repairZero(val){
-  return val<0 ? '0'+val: val
+  return val<10 ? '0'+val: val
 }
 let date = new Date()
 let str = ``
-str += `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${repairZero(date.getMinutes())}:${repairZero(date.getSeconds())}`
+str += `${date.getFullYear()}-${repairZero(date.getMonth()+1)}-${date.getDate()} ${repairZero(date.getHours())}:${repairZero(date.getMinutes())}:${repairZero(date.getSeconds())}`
 console.log(str)
