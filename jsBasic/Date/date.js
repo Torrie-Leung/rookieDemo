b425614bddf912871a8515a715905421ea7cb122
TorrieL <hmminicity2@163.com>
2020-03-13 01:19:53 +0800

    formatter weekday

--- jsBasic/Date/date.js
+++ jsBasic/Date/date.js
@@ -1,7 +1,12 @@
 function repairZero(val){
   return val<10 ? '0'+val: val
 }
+function fmtWeekDay(day){
+  return '星期'+'日一二三四五六'[day]
+}
 let date = new Date()
 let str = ``
 str += `${date.getFullYear()}-${repairZero(date.getMonth()+1)}-${date.getDate()} ${repairZero(date.getHours())}:${repairZero(date.getMinutes())}:${repairZero(date.getSeconds())}`
 console.log(str)
+
+console.log(fmtWeekDay(date.getDay()))
