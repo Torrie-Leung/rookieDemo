9c9f37a231a390f0890cb978f1fd6a3471856db8
TorrieL <hmminicity2@163.com>
2020-03-14 18:45:47 +0800

    calc date

--- jsBasic/Date/date.js
+++ jsBasic/Date/date.js
@@ -10,3 +10,37 @@ str += `${date.getFullYear()}-${repairZero(date.getMonth()+1)}-${date.getDate()}
 console.log(str)
 
 console.log(fmtWeekDay(date.getDay()))
+
+// check date gap
+function calcDate(year,month,day){
+  let totalDay = 0
+  totalDay += day
+  for(let i = 1; i < month; i++){
+    switch(i){
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+          totalDay += 31
+          break
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+          totalDay +=30
+          break
+      case 2:
+        totalDay += 28
+        if((year % 4===0 && year%100!==0) || year % 400 === 0){
+          totalDay += 1
+        }
+        break
+    }
+  }
+  return totalDay
+}
+
+console.log(calcDate(2020,3,30));
