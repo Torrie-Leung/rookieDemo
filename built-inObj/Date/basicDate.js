770712da7d8f7f12cb3790bd2ed15ba878164d2e
TorrieL <hmminicity2@163.com>
2020-03-21 02:39:44 +0800

    encapsulate time display

--- built-inObj/Date/basicDate.js
+++ built-inObj/Date/basicDate.js
@@ -4,6 +4,7 @@ console.log(datee.getFullYear());
 console.log(date1);
 console.log(datee.getMonth());
 console.log(datee.getDay());
+console.log(datee.getHours());
 
 const y = datee.getFullYear()
 const m = datee.getMonth() + 1
@@ -12,3 +13,15 @@ const day = datee.getDay()
 const weekArr = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六']
 
 console.log(`今天是 ${y}年${m}月${date}日${weekArr[day]}`);
+
+function getTime(){
+  const time = new Date()
+  let h = time.getHours()
+  h = h < 10 ? '0'+ h : h
+  let m = time.getMinutes()
+  m = m < 10 ? '0'+ m : m
+  let s = time.getSeconds()
+  s = s < 10 ? '0'+ s : s
+  return `${h}:${m}:${s}`
+}
+console.log(getTime());
