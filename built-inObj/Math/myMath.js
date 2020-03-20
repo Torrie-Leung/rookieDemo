6739d6a1ab84bbb436c14331993c7dd9e8c60ff6
TorrieL <hmminicity2@163.com>
2020-03-20 14:08:32 +0800

    random num between two num

--- built-inObj/Math/myMath.js
+++ built-inObj/Math/myMath.js
@@ -17,7 +17,15 @@ const myMath = {
       }
     }
     return min
+  },
+  randomNum: function(min,max){
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random()*(max-min +1)) + min
+    
   }
 }
 console.log(myMath.max(1,5,9));
 console.log(myMath.min(10,5,9));
+console.log(Math.random());
+console.log(myMath.randomNum(1,100));
