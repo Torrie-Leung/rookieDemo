08f17c4ff2eaedae587050d9bb723fc6ca572661
TorrieL <hmminicity2@163.com>
2020-03-22 01:58:08 +0800

    push&unshift

---
+++ built-inObj/Array/add-del.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3]
+console.log(arr.push(4,'chimp',5));//return the length of new array
+console.log(arr);
+console.log(arr.unshift(100));
+console.log(arr);
--- built-inObj/Array/basicArr.js
+++ built-inObj/Array/basicArr.js
@@ -2,6 +2,7 @@ let arr = new Array(2)
 console.log(arr) // [ <2 empty items> ] 2 stands for arr'length
 let ar = []
 console.log(ar instanceof Array);// true
+console.log(Array.isArray(ar));// true only support IE9+
 
 function reverse(arrr){
   if(arrr instanceof Array){
@@ -10,6 +11,9 @@ function reverse(arrr){
       newArr[newArr.length] = arrr[i]
     }
     return newArr
+  }else {
+    return 'error: please input an array'
   }
 }
-console.log(reverse([1,4,8,9]));
+console.log(reverse([1,4,8,9]));//[ 9, 8, 4, 1 ]
+console.log(reverse(1,2,4));//error: please input an array
