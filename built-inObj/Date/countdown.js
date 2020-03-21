dcc2d8e984be0a6fd84c90837d55528d6b28da77
TorrieL <hmminicity2@163.com>
2020-03-21 13:05:28 +0800

    concat 0 with ternary operation

--- built-inObj/Date/countdown.js
+++ built-inObj/Date/countdown.js
@@ -2,10 +2,14 @@ function countDown(t){
   const nowTime = +new Date()
   const inputTime = +new Date(t)
   let timeInterval = (inputTime - nowTime)/1000 // millisecond transfers to second
-  const d = parseInt(timeInterval/60/60/24)
-  const h = parseInt(timeInterval/60/60%24)
-  const m = parseInt(timeInterval/60%60)
-  const s = parseInt(timeInterval%60)
+  let d = parseInt(timeInterval/60/60/24)
+  d = d < 10 ? '0'+ d : d
+  let h = parseInt(timeInterval/60/60%24)
+  h = h < 10 ? '0'+ h : h
+  let m = parseInt(timeInterval/60%60)
+  m = m < 10 ? '0'+ m : m
+  let s = parseInt(timeInterval%60)
+  s = s < 10 ? '0'+ s : s
   return `${d}天${h}时${m}分${s}秒`
 }
 console.log(countDown('2020-4-30 18:00'));
