f20b16d8b08e5cb3c5d01a22f61102a354d47ebf
TorrieL <hmminicity2@163.com>
2020-03-22 02:00:40 +0800

    pop&shift

--- built-inObj/Array/add-del.js
+++ built-inObj/Array/add-del.js
@@ -3,3 +3,7 @@ console.log(arr.push(4,'chimp',5));//return the length of new array
 console.log(arr);
 console.log(arr.unshift(100));
 console.log(arr);
+console.log(arr.pop());//return the value of deleted ele
+console.log(arr);
+console.log(arr.shift());//return the value of deleted ele
+console.log(arr);
