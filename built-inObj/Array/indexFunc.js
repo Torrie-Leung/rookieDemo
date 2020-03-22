88e0cfc537cff8922778326ea151116a656c85a6
TorrieL <hmminicity2@163.com>
2020-03-22 21:10:51 +0800

    check index

---
+++ built-inObj/Array/indexFunc.js
@@ -0,0 +1,5 @@
+const arr = ['chimp','bender','mario','toad','toad','luigi']
+console.log(arr.indexOf('toad')); // 3 only return the first one which meet the arg
+console.log(arr.indexOf('arr')); // if none ,return -1
+console.log(arr.lastIndexOf('toad')); // 4 find the ele reversely
+console.log(arr.lastIndexOf('arr')); // if none ,return -1
