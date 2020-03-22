b606c664f46f0b366d66e9ef1f91afc0a71eed69
TorrieL <hmminicity2@163.com>
2020-03-22 21:37:26 +0800

    remove duplicate element in an array

---
+++ built-inObj/Array/rmvDuplicateEle.js
@@ -0,0 +1,15 @@
+// We can remove duplicate element in an array by using Temporary Array
+const arr = ['chimp','bender','mario','toad','chimp','toad','luigi']
+
+function uniqueEle(originalArr){
+  let newARR = []
+  for(let i = 0; i< originalArr.length; i++){
+    if(newARR.indexOf(originalArr[i]) === -1){
+      console.log(originalArr[i]);
+      newARR.push(originalArr[i])
+    }
+  }
+  return newARR
+}
+let uniqueArr = uniqueEle(arr)
+console.log(uniqueArr);
