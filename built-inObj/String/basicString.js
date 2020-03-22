00bcbf0431b56b508d1e48ba6c7417f68c2ec7fd
TorrieL <hmminicity2@163.com>
2020-03-22 23:07:32 +0800

    replicated times

--- built-inObj/String/basicString.js
+++ built-inObj/String/basicString.js
@@ -3,3 +3,12 @@ const str = 'chimp'
 const phrase = "We face major issues like economic restructuring, a shrinking workforce, and demographic changes"
 console.log(phrase.indexOf('c'));
 console.log(phrase.indexOf('c', 6));
+
+let idx = phrase.indexOf('c')
+let num = 0
+while(idx !== -1){
+  console.log(idx);
+  num ++
+  idx = phrase.indexOf('c',idx+1)
+}
+console.log(`c in phrase appears ${num} times`);
