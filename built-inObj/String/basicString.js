812e08c77fbf84190d39a009bd6766c2aaa3178a
TorrieL <hmminicity2@163.com>
2020-03-22 22:36:19 +0800

    string indexOf

---
+++ built-inObj/String/basicString.js
@@ -0,0 +1,5 @@
+const str = 'chimp'
+// console.log(str.length);
+const phrase = "We face major issues like economic restructuring, a shrinking workforce, and demographic changes"
+console.log(phrase.indexOf('c'));
+console.log(phrase.indexOf('c', 6));
