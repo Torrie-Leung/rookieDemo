08e318777ff53df8fe97802253bb6fcfedb54f05
TorrieL <hmminicity2@163.com>
2020-03-25 00:34:38 +0800

    class function

--- oop/objClass.js
+++ oop/objClass.js
@@ -1,7 +1,11 @@
 class Star{
-  constructor(urname){
+  constructor(urname,age){
     this.urname = urname
+    this.age = age
+  }
+  code(lang){
+    console.log(`${this.urname} is coding ${lang}.`)
   }
 }
-let chimp = new Star('chimp')
-console.log(chimp);
+let chimp = new Star('chimp',38)
+console.log(chimp.code('vue'));
