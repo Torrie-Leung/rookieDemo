c134e9762e6ca0b7ad2014d1b23fcfd97cb83b0f
TorrieL <hmminicity2@163.com>
2020-03-26 00:18:52 +0800

    tab&tab content toggle

--- oop/tabs.js
+++ oop/tabs.js
@@ -1,19 +1,31 @@
+let that
 class Tabs{
   constructor(id){
     this.main = document.querySelector(id)
     this.lis = this.main.querySelectorAll('li')
     this.sections = this.main.querySelectorAll('section')
     this.init()
+    that = this
+    
   }
   init(){
     for(let i = 0; i < this.lis.length; i++){
       this.lis[i].index = i
-      this.lis[i].onclick = function(){
-        console.log(this.index);
-      }
+      this.lis[i].onclick = this.toggleTab
     }
   }
-  toggleTab(){}
+  toggleTab(){
+    console.log(this.index);
+    that.clearClass()
+    this.className = 'activeli'
+    that.sections[this.index].className = 'activeContent'
+  }
+  clearClass(){
+    for(let i = 0; i < this.lis.length; i++){
+      this.lis[i].className = ''
+      this.sections[i].className = ''
+    }
+  }
   addTab(){}
   rmvTab(){}
   editTab(){}
