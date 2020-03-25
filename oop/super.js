275ee4596dbc7a1e591b7009a6df84071dd85011
TorrieL <hmminicity2@163.com>
2020-03-25 21:58:21 +0800

    extended func & unique func

--- oop/super.js
+++ oop/super.js
@@ -10,7 +10,17 @@ class Beverage{
 class Vita extends Beverage{
   constructor(num,price){
     super(num,price)
+    this.num = num
+    this.price = price
+  }
+  bargain(bill){
+    return bill*0.8
+  }
+  cheat(){
+    return this.num*this.price*1.2
   }
 }
 let vitaLemmon = new Vita(6,1.65)
 console.log(vitaLemmon.bill());
+console.log(vitaLemmon.bargain(vitaLemmon.bill()));
+console.log(vitaLemmon.cheat());
