45749efd9743faaa69382ce5431f58df991662bc
TorrieL <hmminicity2@163.com>
2020-03-26 00:02:26 +0800

    init tabs

--- oop/tabs.js
+++ oop/tabs.js
@@ -3,6 +3,15 @@ class Tabs{
     this.main = document.querySelector(id)
     this.lis = this.main.querySelectorAll('li')
     this.sections = this.main.querySelectorAll('section')
+    this.init()
+  }
+  init(){
+    for(let i = 0; i < this.lis.length; i++){
+      this.lis[i].index = i
+      this.lis[i].onclick = function(){
+        console.log(this.index);
+      }
+    }
   }
   toggleTab(){}
   addTab(){}
