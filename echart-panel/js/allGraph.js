3e8da2084698b667ee8e0695bbdec5ab69a45dfa
TorrieL <hmminicity2@163.com>
2020-04-07 00:32:01 +0800

    vertical progress bar

--- echart-panel/js/allGraph.js
+++ echart-panel/js/allGraph.js
@@ -38,6 +38,7 @@
             fontSize: "12"
           },
           interval:0,
+          rotate:45
         },
         axisLine: {
           show: false
@@ -55,7 +56,8 @@
           textStyle: {
             color: "rgba(255,255,255,.6)",
             fontSize: "12",
-            width:1
+            width:1,
+            
           }
         },
         axisLine: {
@@ -91,5 +93,111 @@
 })();
 
 (function(){
-  let myChart
+  let myChart = echarts.init(document.querySelector('.bar1 .chart'))
+  let data = [70, 34, 60, 78, 69];
+  let titlename = ["HTML5", "CSS3", "javascript", "VUE", "NODE"];
+  let valdata = [702, 350, 610, 793, 664];
+  let myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
+  option = {
+    //图标位置
+    grid: {
+      top: "10%",
+      left: "20%",
+      bottom: "10%"
+    },
+    xAxis: {
+      show: false
+    },
+    yAxis: [
+      {
+        show: true,
+        data: titlename,
+        inverse: true,
+        axisLine: {
+          show: false
+        },
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: "#fff",
+
+          rich: {
+            lg: {
+              backgroundColor: "#339911",
+              color: "#fff",
+              borderRadius: 15,
+              // padding: 5,
+              align: "center",
+              width: 15,
+              height: 15
+            }
+          }
+        }
+      },
+      {
+        show: true,
+        inverse: true,
+        data: valdata,
+        axisLabel: {
+          textStyle: {
+            fontSize: 12,
+            color: "#fff"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "条",
+        type: "bar",
+        yAxisIndex: 0,
+        data: data,
+        barCategoryGap: 50,
+        barWidth: 10,
+        itemStyle: {
+          normal: {
+            barBorderRadius: 20,
+            color: function(params) {
+              // console.log(params);
+              var num = myColor.length;
+              return myColor[params.dataIndex % num];
+            }
+          }
+        },
+        label: {
+          normal: {
+            show: true,
+            position: "inside",
+            formatter: "{c}%"
+          }
+        }
+      },
+      {
+        name: "框",
+        type: "bar",
+        yAxisIndex: 1,
+        barCategoryGap: 50,
+        data: [100, 100, 100, 100, 100],
+        barWidth: 15,
+        itemStyle: {
+          normal: {
+            color: "none",
+            borderColor: "#00c1de",
+            borderWidth: 3,
+            barBorderRadius: 15
+          }
+        }
+      }
+    ]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
 })();
