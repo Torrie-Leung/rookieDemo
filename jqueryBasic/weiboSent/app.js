dfbbbb4a68f9ca37bb34d4cdc638f271aa7a8b3f
TorrieL <hmminicity2@163.com>
2020-04-11 00:23:39 +0800

    jquery event delegation&slideUp&Down

--- jqueryBasic/weiboSent/app.js
+++ jqueryBasic/weiboSent/app.js
@@ -4,5 +4,15 @@ $(function(){
     li.html($('.txt').val() + "<a href='javasript:;'>Delete</a>")
     $('ul').prepend(li)
     li.slideDown()
+    $('.txt').val("")
+
+  })
+  // click to delete
+  // event delegation
+  $('ul').on('click','a',function(){
+    // $(this).parent().slideUp().remove()
+    $(this).parent().slideUp(function(){
+      $(this).parent().remove()
+    })
   })
 })
