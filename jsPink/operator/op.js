9dab1db2f8816cd56b016f041098186acc7b20c4
TorrieL <hmminicity2@163.com>
2020-05-16 11:10:19 +0800

    n++

--- jsPink/operator/op.js
+++ jsPink/operator/op.js
@@ -1,8 +1,11 @@
-//后置递增减
-
+//后置递增减 先返回original value,再自增减1
+let num = 10;
+console.log(num++ +10); // 20
+console.log(num);
 //前置递增减 先自增自减1 再return value
 let num1 = 1;
 console.log(++num1); // num = num + 1
 
 let p = 10; 
 console.log(++p + 10); // 11 + 10 = 21
+console.log(p);
