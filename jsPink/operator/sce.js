46609c6cf4fa2630b01c0d4822530f87850a3b21
TorrieL <hmminicity2@163.com>
2020-05-17 12:52:24 +0800

    short-circuit logical OR

--- jsPink/operator/sce.js
+++ jsPink/operator/sce.js
@@ -7,3 +7,11 @@ console.log(0 && 456); // 0
 console.log('' && 4*56 && 77*56); // 0
 console.log(NaN && 4*56 && 77*56); // NaN
 console.log(undefined && 4*56 && 77*56); // undefined
+
+// ||
+console.log(123 || 456); // 123
+console.log(0 || 456123 || 456); // 456123
+console.log(0 || NaN || 456); // 456
+let score = 0;
+console.log(123 || score++);
+console.log(score);
