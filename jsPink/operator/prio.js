9b321c6d556b315c35530224d7749a7b9a6b25b5
TorrieL <hmminicity2@163.com>
2020-05-17 16:42:05 +0800

    operator priority

---
+++ jsPink/operator/prio.js
@@ -0,0 +1,6 @@
+// false || true  && true && true
+// && > ||
+console.log(4 >= 6 || 'Thesewt' != 'TurnRed' && !(12 * 2 == 144) && true);// true
+let num = 10;
+// true && true
+console.log(5 == num / 2 && ( 2 + 2 * num).toString() === '22'); //true
--- jsPink/operator/sce.js
+++ jsPink/operator/sce.js
@@ -2,7 +2,7 @@
 // when evaluating boolean expressions(logical AND and OR) you can stop as soon as you find the first condition which satisfies or negates the expression.
 
 // &&
-console.log(123 && 456); // 456
+console.log(123 && 456 && 'Thesewt'); // Thesewt
 console.log(0 && 456); // 0
 console.log('' && 4*56 && 77*56); // 0
 console.log(NaN && 4*56 && 77*56); // NaN
