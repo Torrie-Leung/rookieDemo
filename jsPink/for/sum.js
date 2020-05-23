0d802a850b3fbb28d189c41980d9c73d06ea6a12
TorrieL <hmminicity2@163.com>
2020-05-23 10:17:47 +0800

    odd sum even sum

--- jsPink/for/sum.js
+++ jsPink/for/sum.js
@@ -7,3 +7,15 @@ for(let i = 0; i <= itemNum; i++){
 avg = sum/itemNum;
 console.log({sum});
 console.log({avg});
+
+let even = 0;
+let odd = 0;
+for(let i = 1; i <= itemNum; i++){
+  if(i % 2 == 0){
+    even += i;
+  }else{
+    odd += i;
+  }
+}
+console.log({evenSum:even})
+console.log({oddSum:odd})
