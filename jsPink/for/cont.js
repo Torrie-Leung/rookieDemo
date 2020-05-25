830c676e9d2bea89024c1552f622da06230d021f
TorrieL <hmminicity2@163.com>
2020-05-25 22:09:10 +0800

    continue

---
+++ jsPink/for/cont.js
@@ -0,0 +1,16 @@
+for(let i = 1; i <= 5; i++){
+  if(i ===3 ){
+    // 退出第3次循环
+    continue;
+  }
+  console.log(`hit you ${i} times!`)
+}
+// 1~100之间除了能整除7之外的数的和
+let sum = 0;
+for(let i = 1; i <= 100; i++){
+  if( i % 7 == 0){
+    continue;
+  }
+  sum += i;
+}
+console.log({sum})
