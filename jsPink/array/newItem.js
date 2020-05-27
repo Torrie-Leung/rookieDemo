c23078b7623c750e5ebc6748283f8ae56398400b
TorrieL <hmminicity2@163.com>
2020-05-28 06:58:15 +0800

    add item by counter

--- jsPink/array/newItem.js
+++ jsPink/array/newItem.js
@@ -3,3 +3,13 @@ for(let i = 0; i < 10; i++){
   arr[i] = i + 1;
 }
 console.log({arr});
+
+let newArr = [];
+let oldArr = [1,5,77,3,7,99] // pick out the number bigger than 10
+let j = 0;
+for(let i = 0; i < oldArr.length; i++){
+  if(oldArr[i] > 10){
+    newArr[j] = oldArr[i];
+    j++;
+  }
+}
