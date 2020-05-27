bd74526f27038e6cfd4e9f8a6585ee7b9c8c85b7
TorrieL <hmminicity2@163.com>
2020-05-28 07:03:07 +0800

    add item by new array length

--- jsPink/array/newItem.js
+++ jsPink/array/newItem.js
@@ -8,8 +8,16 @@ let newArr = [];
 let oldArr = [1,5,77,3,7,99] // pick out the number bigger than 10
 let j = 0;
 for(let i = 0; i < oldArr.length; i++){
+  // solution 1
+  // if(oldArr[i] > 10){
+  //   newArr[j] = oldArr[i];
+  //   j++;
+  // }
+
+  // solution 2
   if(oldArr[i] > 10){
-    newArr[j] = oldArr[i];
-    j++;
+    newArr[newArr.length] = oldArr[i];
+    
   }
 }
+console.log({newArr});
