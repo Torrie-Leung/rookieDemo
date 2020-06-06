3a54d2d620f8c06e58da0b0f9ddf676764d49d49
TorrieL <hmminicity2@163.com>
2020-06-06 14:27:25 +0800

    obj created

---
+++ jsPink/Object/constructObj.js
@@ -0,0 +1,7 @@
+// constructor function first letter should be capital
+function Stars(name,pro,level){
+  this.name = name
+  this.pro = pro
+  this.level = level
+}
+new Stars('Bender','Sarcasm',230)
---
+++ jsPink/Object/creObj.js
@@ -0,0 +1,10 @@
+var obj = {
+  uname: 'chimp',
+  age: 38,
+  salary: 2300,
+  dissU: function(){
+    console.log('ew~');
+  }
+}
+console.log(obj['salary']);
+obj.dissU()
