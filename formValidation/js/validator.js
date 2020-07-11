d5a422e2516854a973ba4c7e0d29010f194a4749
TorrieL <hmminicity2@163.com>
2020-07-11 19:48:51 +0800

    encapsulate validator

--- formValidation/js/validator.js
+++ formValidation/js/validator.js
@@ -3,16 +3,39 @@ $(function(){
   window.Validator = function(val,rule){
 
     this.validate_max = function(){
-      val = parseFloat(val);
+      pre_max_min()
       return val <= rule.max
     }
     this.validate_min = function(){
-      val = parseFloat(val);
+      pre_max_min()
       return val >= rule.min
     }
     this.validate_maxlength = function(){
-      val = val.toString();
+      pre_length()
       return val.length <= rule.maxlength
+    }
+    this.validate_minlength = function(){
+      pre_length()
+      return val.length >= rule.minlength
+    }
+    this.validate_numeric = function(){
+      return $.isNumeric(val);
+    }
+    this.validate_nullable = function(){
+      var real = $.trim(val)
+      if(!real && real !== 0){
+        return false;
+      }
+      return true;
+    }
+    // for this.validate_max or this.validate_min
+    function pre_max_min(){
+      val = parseFloat(val);
+    }
+
+    // for this.validate_maxlength or this.validate_minlength
+    function pre_length(){
+      val = val.toString();
     }
   }
 })
