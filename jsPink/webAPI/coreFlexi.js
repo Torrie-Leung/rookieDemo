dc4b05bcb40fee429d492a246d435624cf5b4eae
TorrieL <hmminicity2@163.com>
2020-07-20 22:55:15 +0800

    dpr

--- jsPink/webAPI/coreFlexi.js
+++ jsPink/webAPI/coreFlexi.js
@@ -1,1 +1,17 @@
-(function flexible(window,document){})(window,document);
+(function flexi(window, document){
+  // 获取html根元素
+  var docEl = document.documentElement
+  // dpr物理像素比
+  var dpr = window.devicePixelRatio || 1
+  console.log(dpr);
+  // adjust body font size
+  function setBodyFontSize(){
+    if(document.body){
+      document.body.style.fontSize = (12 * dpr) + 'px'
+    }else{
+      // js文件引用在body前
+      document.addEventListener('DOMContentLoaded', setBodyFontSize)
+    }
+  }
+  setBodyFontSize();
+}(window, document))
