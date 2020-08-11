062474b84afa1962e709f4ffdb82b31c84811cf4
TorrieL <hmminicity2@163.com>
2020-08-11 19:27:21 +0800

    condense task

--- Node-bh/gulp-demo/gulpfile.js
+++ Node-bh/gulp-demo/gulpfile.js
@@ -1,6 +1,19 @@
 const gulp = require('gulp')
 //setup task
+//区分多个任务的依据就是任务名称
+//需要async await
 gulp.task('first', async() =>{
   await gulp.src('./src/teststyle.css')
       .pipe(gulp.dest('./dist/css'))
+})
+
+//html mission
+//1. compress
+//2. extract common code
+const htmlmin = require('gulp-htmlmin')
+gulp.task('htmlmin', async() => {
+  await gulp.src('./src/*.html')
+      //压缩html中的代码
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist'))
 })
