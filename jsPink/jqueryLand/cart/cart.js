d2bbbf273d674e898e9f5e083d05c36925f61158
TorrieL <hmminicity2@163.com>
2020-09-06 01:05:24 +0800

    multi selector

--- jsPink/jqueryLand/cart/cart.js
+++ jsPink/jqueryLand/cart/cart.js
@@ -2,6 +2,6 @@ $(function(){
 // check all uncheck all
 //把全选按钮的状态赋值给商品中的按钮
   $(".checkAll").change(function(){
-    $(".j-checkobx").prop("checked",$(this).prop("checked")) 
+    $(".j-checkobx,.checkAll").prop("checked",$(this).prop("checked")) 
   })
 });
