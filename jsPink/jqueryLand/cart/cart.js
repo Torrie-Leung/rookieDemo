2d9f2506d01396cccc9aed60fcbad008dcf6399d
TorrieL <hmminicity2@163.com>
2020-09-06 01:11:45 +0800

    reverse chekall

--- jsPink/jqueryLand/cart/cart.js
+++ jsPink/jqueryLand/cart/cart.js
@@ -1,7 +1,17 @@
 $(function(){
-// check all uncheck all
-//把全选按钮的状态赋值给商品中的按钮
+  // check all uncheck all
+  //把全选按钮的状态赋值给商品中的按钮
   $(".checkAll").change(function(){
     $(".j-checkobx,.checkAll").prop("checked",$(this).prop("checked")) 
+  })
+  // 通过小复选框控制全选
+  
+  $(".j-checkobx").change(function(){
+    var itemNum = $(".j-checkobx").length
+    if($(".j-checkobx:checked").length === itemNum ){
+      $(".checkAll").prop("checked",true) 
+    }else{
+      $(".checkAll").prop("checked",false) 
+    }
   })
 });
