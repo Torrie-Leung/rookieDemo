0e17bc1f920bddc824f15e5411c170dec309f104
TorrieL <hmminicity2@163.com>
2021-01-02 12:59:34 +0800

    dummy call fuction

--- inherit/index.js
+++ inherit/index.js
@@ -1,11 +1,34 @@
 // es5 inheritance
-function Root(){
-  this.name = 'root1'
+function Root(num){
+  this.name = 'root1';
+  this.num = num
+  this.motion = function(){
+    console.log(this.num);
+  }
 }
 
+
 function Branch(){
-  Root.call(this) // call 方法把Root中作用域指定到Branch里来
+  Root.call(this) // call 方法把Root中作用域指定到Branch里来,只能继承属性值
   this.type = 'branch1'
 }
 
-console.log(new Branch);
+// console.log(new Branch);
+// Branch.digHole(1)
+
+// prototype
+function BranchPro(){
+  this.type = 'branchpro'
+}
+BranchPro.prototype = new Root(3)
+console.log(new BranchPro);
+
+function User(name,age){
+  this.name = name;
+  this.age = age;
+  this.display = function(){
+    console.log(this.name + ' '+this.age +'year-old');
+  }
+}
+let passerby = new User('passerby',35)
+passerby.display();
