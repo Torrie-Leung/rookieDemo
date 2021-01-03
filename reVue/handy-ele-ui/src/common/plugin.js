b147b107422e4f64b01933732690cce4e8cf950d
TorrieL <hmminicity2@163.com>
2021-01-03 23:22:11 +0800

    obj default definition

--- reVue/handy-ele-ui/src/common/plugin.js
+++ reVue/handy-ele-ui/src/common/plugin.js
@@ -1,7 +1,19 @@
 let Toast = {}
 Toast.install = function(Vue, options){
   let opt = {
-    // define default position
-    defaultType: 'center',
+    defaultType: 'center',// define default position
+    duration: '1500',// default duartaion
+  }
+  for(let prop in options){
+    opt[prop] = options[prop]
+  }
+  Vue.prototype.$toast = (tips,type) =>{
+    if(type){
+      opt.defaultType = type
+    }
+    // if(tips){}
+    let toastTpl = Vue.extend({
+      template: `<div class="vue-toast toast-${opt.defaultType}">${tips}</div>`
+    })
   }
 }
