a96982e856b37840afd69f213147509373f27806
TorrieL <hmminicity2@163.com>
2021-01-24 12:53:15 +0800

    basic nice loop

--- back2B/array/arr.js
+++ back2B/array/arr.js
@@ -3,3 +3,16 @@ let myList = ['Medical','Consumption','Tech']
 myList.forEach(function(value,index){
   console.log(`This is No.${index} ${value} fund.`);
 })
+
+// while
+let times = 0;
+while (times < 10) {
+  console.log('tried it', times);
+  times++
+}
+
+// for(setup,comparison,change)
+for(var i = 0; i < myList.length; i++){
+  // console.log('i is ',i);
+  console.log(`you have ${myList[i]} in your basket`);
+}
