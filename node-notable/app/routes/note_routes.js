25a9de5dc971dfc6c774710027d25ee04e85782c
TorrieL <hmminicity2@163.com>
2021-01-31 19:43:18 +0800

    type get api store data to mongodb

--- node-notable/app/routes/note_routes.js
+++ node-notable/app/routes/note_routes.js
@@ -1,7 +1,16 @@
 module.exports = function(app, db){
+  // post
   app.post('/notes', (req,res) => {
     // create the note here
-    console.log(req.body);
-    res.send('hey dummy~')
+    // console.log(req.body);
+    // res.send('hey dummy~')
+    const note = {text: req.body.body, title: req.body.title};
+    db.collection('notes').insertOne(note,(err, result) => {
+      if(err){
+        res.send({'err':'err occured'})
+      }else{
+        res.send(result.ops[0])
+      }
+    })
   })
 }
---
+++ node-notable/config/db.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  url : "mongodb+srv://dbUsr:base2300@base.xdpzc.mongodb.net/Base?retryWrites=true&w=majority"
+}
+
