6b3a4e428fce2fedeb070f9aa4f4bd0cb6c3edc2
TorrieL <hmminicity2@163.com>
2021-01-31 20:41:03 +0800

    put&delete

--- node-notable/app/routes/note_routes.js
+++ node-notable/app/routes/note_routes.js
@@ -1,4 +1,17 @@
+let ObjectID = require('mongodb').ObjectID
 module.exports = function(app, db){
+  // get
+  app.get('/notes/:id',(req,res)=>{
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    db.collection('notes').findOne(details,(err,item) => {
+      if(err){
+        res.send({'err:':'an err has occured'});
+      }else{
+        res.send(item);
+      }
+    })
+  })
   // post
   app.post('/notes', (req,res) => {
     // create the note here
@@ -7,9 +20,37 @@ module.exports = function(app, db){
     const note = {text: req.body.body, title: req.body.title};
     db.collection('notes').insertOne(note,(err, result) => {
       if(err){
-        res.send({'err':'err occured'})
+        res.send({'err':'err occured'});
       }else{
-        res.send(result.ops[0])
+        res.send(result.ops[0]);
+      }
+    })
+  })
+
+  // put
+  // get
+  app.put('/notes/:id',(req,res)=>{
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    const note = {text: req.body.body, title: req.body.title};
+    db.collection('notes').update(details,note,(err,item) => {
+      if(err){
+        res.send({'err:':'an err has occured'});
+      }else{
+        res.send(item);
+      }
+    })
+  })
+
+  // delete
+  app.delete('/notes/:id',(req,res)=>{
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    db.collection('notes').remove(details,(err,item) => {
+      if(err){
+        res.send({'err:':'an err has occured'});
+      }else{
+        res.send('Note '+ id + ' deleted!');
       }
     })
   })
