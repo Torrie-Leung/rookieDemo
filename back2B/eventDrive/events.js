694cedc4be33081fcbd6ddb61b7aa9961941a752
TorrieL <hmminicity2@163.com>
2021-02-06 00:42:53 +0800

    input blur event

--- back2B/eventDrive/events.js
+++ back2B/eventDrive/events.js
@@ -1,8 +1,10 @@
 var checklist = document.getElementById('checklist')
 var items = checklist.querySelectorAll('li')
+var inputs = checklist.querySelectorAll('input')
 
 for(var i = 0; i < items.length; i++){
   items[i].addEventListener('click',editItem)
+  inputs[i].addEventListener('blur',updateItem)
 }
 
 function editItem(){
@@ -10,4 +12,7 @@ function editItem(){
   var input = this.querySelector('input')
   input.focus()
   input.setSelectionRange(0,input.value.length);
+}
+function updateItem(){
+  console.log(this.value);
 }
